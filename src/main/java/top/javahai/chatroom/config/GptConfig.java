b12a60f64cf9344ac806e0699e54da8ea3dafdd2
Ciward <2273805191@qq.com>
2024-11-02 20:41:31 +0800

    11

--- src/main/java/top/javahai/chatroom/config/GptConfig.java
+++ src/main/java/top/javahai/chatroom/config/GptConfig.java
@@ -40,7 +40,79 @@ public class GptConfig {
 
     @Value("${app.server.ip}")
     private String serverIp;
+    public static String getToken() {
+        String token = "";
+        String url = "https://aip.baidubce.com/oauth/2.0/token?client_id=ML2S2Kmn7e6rVAE2OBC1g0RG&client_secret=HuYbCQeGtbmcbiPjBflaQOfKuuZH6GiM&grant_type=client_credentials";
+        try {
+            token = HttpUtils.httpPost(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject = JSONObject.parseObject(token);
+        if (jsonObject != null && jsonObject.containsKey("access_token")) {
+            return jsonObject.getString("access_token");
+        } else {
+            return null; // 返回空值或者其他默认值，表示获取 token 失败
+        }
+    }
+    public static String getMessage(String content) {
+        String requestMethod = "POST";
+        String url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions?access_token="+getToken();//post请求时格式
+        HashMap<String, String> msg = new HashMap<>();
+        msg.put("role","user");
+        msg.put("content",content);
+        ArrayList<HashMap> messages = new ArrayList<>();
+        messages.add(msg);
+        HashMap<String, Object> requestBody = new HashMap<>();
+        requestBody.put("messages", messages);
+        String outputStr = JSON.toJSONString(requestBody);
+        JSON json = HttpRequest.httpRequest(url,requestMethod,outputStr,"application/json");
+        JSONObject jsonObject = JSONObject.parseObject(json.toJSONString());
+        String result = jsonObject.getString("result");
+        System.out.println(result);
+        return result;
+    }
 
+    public static List<String> preMessage(String content) {
+        String requestMethod = "POST";
+        String url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions?access_token="+getToken();//post请求时格式
+        HashMap<String, String> msg = new HashMap<>();
+        msg.put("role","user");
+        content = "请将下面一句话提取关键信息，将结果以分词的形式展示出来。下面是我的输入文本："+content;
+        msg.put("content",content);
+        ArrayList<HashMap> messages = new ArrayList<>();
+        messages.add(msg);
+        HashMap<String, Object> requestBody = new HashMap<>();
+        requestBody.put("messages", messages);
+        String outputStr = JSON.toJSONString(requestBody);
+        JSON json = HttpRequest.httpRequest(url,requestMethod,outputStr,"application/json");
+        JSONObject jsonObject = JSONObject.parseObject(json.toJSONString());
+        List<String> result = Arrays.asList(jsonObject.getString("result").split(" "));
+        System.out.println(result);
+        return result;
+    }
+
+    public static String resMessage(String question,List<String> content1) {
+        String requestMethod = "POST";
+        String url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions?access_token="+getToken();//post请求时格式
+        HashMap<String, String> msg = new HashMap<>();
+        msg.put("role","user");
+        String content = String.join("\n", content1);//将所有匹配到的信息存入
+        //用GPT来进行匹配
+        content = "请在下面的数据表中找到最符合问题的答案,如果找不到，那么从网络中搜索答案输出。问题："+question+"\n数据表：\n"+content;
+        System.out.println(content);
+        msg.put("content",content);
+        ArrayList<HashMap> messages = new ArrayList<>();
+        messages.add(msg);
+        HashMap<String, Object> requestBody = new HashMap<>();
+        requestBody.put("messages", messages);
+        String outputStr = JSON.toJSONString(requestBody);
+        JSON json = HttpRequest.httpRequest(url,requestMethod,outputStr,"application/json");
+        JSONObject jsonObject = JSONObject.parseObject(json.toJSONString());
+        String result = jsonObject.getString("result");
+        System.out.println(result);
+        return result;
+    }
 
 
     /**
